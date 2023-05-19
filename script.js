1384c2e3368bc33aee08bb2a2e47f815bff93053
Sebi <sebastianramord@gmail.com>
2023-05-18 20:18:23 -0500

    Added a new function to script.js Basic fn that takes in the rock, paper, scissors input even if it's case insensitive.

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
 
     if (rand === 1) {
-        console.log(`${rock}!`)
+        return rock;
     } else if (rand === 2) {
-        console.log(`${paper}!`)
+        return paper;
     } else {
-        console.log (`${scissors}!`)
+        return scissors;
     }
 }
 
-getComputerChoice();
+function playerSelection(rps) {
 
+    getComputerChoice();
+
+    let choice = rps.toLowerCase();
+
+    if (choice == "rock") {
+        console.log("works")
+    }
+
+}
+
+playerSelection("rock");
