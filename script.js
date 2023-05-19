667e94e840cbcffe5cf96b1962c3feb927152104
Sebi <sebastianramord@gmail.com>
2023-05-18 21:23:27 -0500

    Added the main functions of the game. Pretty much everything on the JS side, all that's left is comment this and test for bugs.

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-const rock = "Rock"
-const paper = "Paper"
-const scissors = "Scissors"
+let rock = "Rock";
+let paper = "Paper";
+let scis = "Scissors";
 
+let computerChoice;
+let playerChoice;
 
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3) + 1;
 
     if (rand === 1) {
-        return rock;
+        computerChoice = rock;
     } else if (rand === 2) {
-        return paper;
+        computerChoice = paper;
     } else {
-        return scissors;
+        computerChoice = scis;
     }
 }
 
-function playerSelection(rps) {
 
-    getComputerChoice();
+function getPlayerChoice(choice = "") {
 
-    let choice = rps.toLowerCase();
+    let processedWord = choice.toLowerCase();
 
-    if (choice == "rock") {
-        console.log("works")
+    if (processedWord === "rock") {
+        playerChoice = rock;
+    } else if (processedWord === "paper") {
+        playerChoice = paper;
+    } else if (processedWord === "scissors") {
+        playerChoice = scis;
     }
 
 }
 
-playerSelection("rock");
+function game() {
+
+    let win = `You win! ${playerChoice} beats ${computerChoice}!`;
+    let lose = `You lose... ${computerChoice} beats ${playerChoice}.`;
+    let tie = `It's a tie! ${computerChoice} against ${playerChoice}!`;
+
+    if (playerChoice === scis && computerChoice === paper) {
+        console.log(win);
+    } else if (playerChoice === rock && computerChoice === paper) {
+        console.log(win);
+    } else if (playerChoice === paper && computerChoice === rock) {
+        console.log(win);
+    } else if (playerChoice === computerChoice) {
+        console.log(tie);
+    } else {
+        console.log(lose);
+    }
+}
+
+getComputerChoice();
+getPlayerChoice("scissors");
+game();
+
+console.log(`\nDebugger...\nComputer Picked: ${computerChoice}\nPlayer Picked: ${playerChoice}`);
