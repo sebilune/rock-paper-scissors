ce51909b1cebfaf81a24a658a455830170cec075
sebiram <sebastianramord@gmail.com>
2023-05-30 03:30:39 -0500

    Improved functuons complexity. Improved the complexity of the game functions by simplifying them using data structures.This was done on all functions

--- script.js
+++ script.js
@@ -3,72 +3,58 @@
 █▀▄ ▀▄▀ ▀▄▄ █ █    █▀  █▀█ █▀  █▄▄ █▀▄    ▄█▀ ▀▄▄ █ ▄█▀ ▄█▀ ▀▄▀ █▀▄ ▄█▀ 
 */
 
-// Define the choices of the game.
-let rock = "Rock";
-let paper = "Paper";
-let scis = "Scissors";
-
 // Define the variables that will hold the choices.
 let computerChoice;
 let playerChoice;
 
 function getComputerChoice() {
-  // Get a random num between one and three.
-  let rand = Math.floor(Math.random() * 3) + 1;
+  const choices = {
+    1: "rock",
+    2: "paper",
+    3: "scissors"
+  };
 
-  // Assign the 3 choices of the game to the computerChoice var depending on the num (1-3).
-  if (rand === 1) {
-    computerChoice = rock;
-  } else if (rand === 2) {
-    computerChoice = paper;
-  } else {
-    computerChoice = scis;
-  }
+  let rand = Math.floor(Math.random() * 3) + 1;
+  computerChoice = choices[rand];
 }
 
 // Get the input from the player, takes onse string as a param.
 function getPlayerChoice(choice = "") {
-  // Turn the input into lowercase, so words like "rOcK" or "PAPER" can still be inputted.
-  let processedWord = choice.toLowerCase();
+  const choices = {
+    rock: "rock",
+    paper: "paper",
+    scissors: "scissors"
+  };
 
-  // Assign what the player chose to the playerChoice variable.
-  if (processedWord === "rock") {
-    playerChoice = rock;
-  } else if (processedWord === "paper") {
-    playerChoice = paper;
-  } else if (processedWord === "scissors") {
-    playerChoice = scis;
-  }
+  let processedWord = choice.toLowerCase();
+  playerChoice = choices[processedWord] || null;
 }
 
-function game() {
-  // Declare the common strings (So I don't have to write them over and over).
-  let win = `You win! ${playerChoice} beats ${computerChoice}!`;
-  let lose = `You lose... ${computerChoice} beats ${playerChoice}.`;
-  let tie = `It's a tie! ${computerChoice} against ${playerChoice}!`;
 
-  // If the player wins, console log the win str.
-  if (
-    (playerChoice === scis && computerChoice === paper) ||
-    (playerChoice === rock && computerChoice === scis) ||
-    (playerChoice === paper && computerChoice === rock)
-  ) {
-    console.log(win);
-  } else if (playerChoice === computerChoice) {
-    // If it is a tie, console log the tie str.
-    console.log(tie);
-  } else {
-    // If none of the above apply, which can only be a lost from the player, then log the lose str.
-    console.log(lose);
-  }
+const outcomes = {
+  rock: {
+    rock: "It's a tie! rock against rock!",
+    paper: "You lose... paper beats rock.",
+    scissors: "You win! rock beats scissors!",
+  },
+  paper: {
+    rock: "You win! paper beats rock!",
+    paper: "It's a tie! paper against paper!",
+    scissors: "You lose... scissors beats paper.",
+  },
+  scissors: {
+    rock: "You lose... rock beats scissors.",
+    paper: "You win! scissors beats paper!",
+    scissors: "It's a tie! scissors against scissors!",
+  },
+};
+
+function game(player, computer) {
+  const outcome = outcomes[player][computer];
+  console.log(outcome);
 }
 
 // Call the functions.
 getComputerChoice();
 getPlayerChoice("sCisSoRs"); // Player input.
-game();
-
-// Debug.
-// console.log(
-//   `\nDebugger...\nComputer Picked: ${computerChoice}\nPlayer Picked: ${playerChoice}`
-// );
+game(playerChoice, computerChoice);
